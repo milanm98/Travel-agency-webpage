3255910277793626c69787ee7611461d3e0b3927
milanm98 <marjanovmilan13@gmail.com>
2021-05-18 15:01:17 +0200

    offers cards

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ fetch("public/imageSlider.json")
 
 var slideIndex = 1;
 ShowSlides(slideIndex);
+ShowTabs("regular");
 
 function plusSlides(n){
     ShowSlides(slideIndex += n);
@@ -48,4 +49,43 @@ function ShowSlides(n){
 
     slides[slideIndex-1].style.display = "block";
     descriptions[slideIndex-1].style.display = "block";
+}
+
+function ShowTabs(TypeOfTab){
+    var regularSpan = document.getElementById("card-navbar-regular");
+    var plusSpan = document.getElementById("card-navbar-plus");
+    var premiumSpan = document.getElementById("card-navbar-premium");
+    var i;
+
+    var tabs = document.getElementsByClassName("card");
+    for(i = 0; i< tabs.length; i++){
+        tabs[i].style.display = "none";
+        regularSpan.style.backgroundColor = "lightgray";
+        regularSpan.style.borderTop = "none";
+
+        plusSpan.style.backgroundColor = "lightgray";
+        plusSpan.style.borderTop = "none"
+        
+        premiumSpan.style.backgroundColor = "lightgray";
+        premiumSpan.style.borderTop = "none"
+
+    }
+
+    if(TypeOfTab === "regular"){
+        tabs[0].style.display = "flex";
+        regularSpan.style.backgroundColor = "white";
+        regularSpan.style.borderTop = "3px solid blue";
+    }
+    else if(TypeOfTab === "plus"){
+        tabs[1].style.display = "flex";
+        plusSpan.style.backgroundColor = "white";
+        plusSpan.style.borderTop = "3px solid blue";
+
+    }
+    else if(TypeOfTab === "premium"){
+        tabs[2].style.display = "flex";
+        premiumSpan.style.backgroundColor = "white";
+        premiumSpan.style.borderTop = "3px solid blue";
+
+    }
 }
